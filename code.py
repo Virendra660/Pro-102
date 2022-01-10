dfa9dbe476db331f5f83e982dcf65d44636b652b
Virendra660 <78679634+Virendra660@users.noreply.github.com>
2022-01-10 19:31:14 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,28 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_snapshot():
+    number = random.randint(0, 100)
+   
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        
+        ret, frame = videoCaptureObject.read()
+        
+        image_name = "img" + str(number) + ".png"
+        cv2.imwrite(image_name, frame)
+        start_time = time.time
+        result = False
+    return image_name
+    print("snapshot taken")
+
+    videoCaptureObject.release()
+
+    cv2.destroyAllWindows()
+
+take_snapshot()
